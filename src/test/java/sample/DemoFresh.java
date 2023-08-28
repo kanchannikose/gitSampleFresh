1b34d060127022790427231803e62aa8d1e0ec51
KanchanQsp <kanchannikose9@gmail.com>
2023-08-28 15:29:04 +0530

    Demo is commited

---
+++ src/test/java/sample/DemoFresh.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoFresh 
+{
+
+	@Test
+	public void sample()
+	{
+		System.out.println("Hi this is my 1st commit");
+		
+	}
+
+}
+
+
